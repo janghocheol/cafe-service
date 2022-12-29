a9764087f2f392fd81ba6092e53d18d5c9c40576
janghocheol <jang806@daum.net>
2022-12-29 07:28:13 +0000

    CancelPayment 폴리시 구현 및 승인코드 저장 로직 추가

--- payment/src/main/java/cafe/service/domain/Payment.java
+++ payment/src/main/java/cafe/service/domain/Payment.java
@@ -8,6 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PostLoad;
 import javax.persistence.PostPersist;
+import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
@@ -15,44 +16,50 @@ import cafe.service.PaymentApplication;
 import cafe.service.external.PGDemo;
 import lombok.Data;
 
-
 @Entity
-@Table(name="Payment_table")
+@Table(name = "Payment_table")
 @Data
 
-public class Payment  {
+public class Payment {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long paymentId;
-    
+
     private Long customerId;
-    
+
     private Long cafeId;
-    
+
     private Long menuId;
-    
+
     private Integer qty;
-    
+
     private Long totalPrice;
-    
+
     private Date orderDate;
-    
+
     private Long approvalCode;
 
     private String status;
-    
+
     private Long orderId;
 
     @PostPersist
-    public void onPostPersist(){
-        pg().startTransaction();
+    public void onPostPersist() {
         Paid paid = new Paid(this);
         paid.publishAfterCommit();
 
     }
-    @PreUpdate
-    public void onPreUpdate(){
 
+    @PrePersist
+    public void onPrePersist() {
+        Long approvalCode = pg().startTransaction();
+        if(approvalCode!=0){
+            setApprovalCode(approvalCode);
+        }
+    }
+
+    @PreUpdate
+    public void onPreUpdate() {
 
         PaymentCanceled paymentCanceled = new PaymentCanceled(this);
         paymentCanceled.publishAfterCommit();
@@ -60,7 +67,7 @@ public class Payment  {
     }
 
     @PostLoad
-    public void makeDelay(){
+    public void makeDelay() {
         try {
             Thread.currentThread().sleep((long) (400 + Math.random() * 220));
         } catch (InterruptedException e) {
@@ -68,48 +75,28 @@ public class Payment  {
         }
     }
 
-
-    public static PaymentRepository repository(){
+    public static PaymentRepository repository() {
         PaymentRepository paymentRepository = PaymentApplication.applicationContext.getBean(PaymentRepository.class);
         return paymentRepository;
     }
 
-    public static PGDemo pg(){
+    public static PGDemo pg() {
         PGDemo pg = PaymentApplication.applicationContext.getBean(PGDemo.class);
         return pg;
     }
 
-
-
-
-    public static void cancelPayment(CustomerOrderCanceled customerOrderCanceled){
-        repository().findByOrderId(customerOrderCanceled.getOrderId()).ifPresent(payment->{
-            
+    public static void cancelPayment(CustomerOrderCanceled customerOrderCanceled) {
+        repository().findByOrderId(customerOrderCanceled.getOrderId()).ifPresent(payment -> {
             payment.setStatus("PaymentCanceled");
             repository().save(payment);
-         });
+        });
     }
-    public static void cancelPayment(OrderCanceled orderCanceled){
 
-        /** Example 1:  new item 
-        Payment payment = new Payment();
-        repository().save(payment);
-
-        */
-
-        /** Example 2:  finding and process
-        
-        repository().findById(orderCanceled.get???()).ifPresent(payment->{
-            
-            payment // do something
+    public static void cancelPayment(OrderCanceled orderCanceled) {
+        repository().findByOrderId(orderCanceled.getOrderId()).ifPresent(payment -> {
+            payment.setStatus("PaymentCanceled");
             repository().save(payment);
-
-
-         });
-        */
-
-        
+        });
     }
-
 
 }
